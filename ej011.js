42ef9ec169c32cb3cc49c6110edfd0aa76a0afe6
birdsinyourgarden <ruizpaloma9@gmail.com>
2024-10-09 10:35:48 +0200

    exercise 011

--- ej011.js
+++ ej011.js
@@ -6,3 +6,20 @@
  * Posible salida: [105, 31, 14, 1, 2, 50, 24, 5]
  */
 
+function shuffleArray(arr) {
+    const shuffledArray = [];
+    const arrCopy = arr.slice();
+
+    while (arrCopy.length > 0) {
+        const randomIndex = Math.floor(Math.random() * arrCopy.length);
+        shuffledArray.push(arrCopy[randomIndex]);
+        arrCopy.splice(randomIndex, 1);
+    }
+    return shuffledArray;
+}
+
+// Ejemplo 
+
+const input = [1, 2, 5, 14, 24, 31, 50, 105];
+const output = shuffleArray(input);
+console.log(output);
--- ej012.js
+++ ej012.js
@@ -5,5 +5,4 @@
  * Elaborar una función que reciba un número y adivine un número entre el 1 y el 100 en el menor número de pasos posibles
  * Elaborar una función que reciba un entero (entre el 100 y el 200) y determinar si es múltiplo de 3
  * Elaborar una función que reciba 3 números y determinar sí uno es la suma de los otros dos
- * Elabora una función que reciba un número y determinar si es par o impar
  */
