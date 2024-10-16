492941c8fb15dc1369fbfade8405bf30e9038934
samuel-un <samuelun00@gmail.com>
2024-10-16 09:01:28 +0200

    Added arraysObjects folder from main

---
+++ arraysObjects/ejercicio4.js
@@ -0,0 +1,54 @@
+// Ejercicio: Uso del método find en JavaScript
+
+// 1. Creamos un array de objetos que representan libros
+
+/* const libros = [
+    {
+        titulo: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        año: 1967,
+        genero: "Realismo mágico"
+    },
+    {
+        titulo: "1984",
+        autor: "George Orwell",
+        año: 1949,
+        genero: "Distopía"
+    },
+    {
+        titulo: "El Principito",
+        autor: "Antoine de Saint-Exupéry",
+        año: 1943,
+        genero: "Fábula"
+    },
+    {
+        titulo: "Orgullo y prejuicio",
+        autor: "Jane Austen",
+        año: 1813,
+        genero: "Romance"
+    },
+    {
+        titulo: "Crónica de una muerte anunciada",
+        autor: "Gabriel García Márquez",
+        año: 1981,
+        genero: "Novela"
+    }
+]; */
+
+// 2. Función para encontrar un libro por su ID
+
+
+// 3. Función para encontrar un libro por su título (ignorando mayúsculas/minúsculas)
+
+
+
+// 4. Función para encontrar el primer libro de un autor específico
+
+
+// 5. Función para encontrar el primer libro publicado después de un año específico
+
+
+// 6. Función para encontrar un libro que cumpla múltiples criterios
+
+
+// 7. Intentar encontrar un libro que no existe
