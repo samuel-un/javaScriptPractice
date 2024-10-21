9f939cf9c4a7b5508d8fb42cf894ce5362636040
birdsinyourgarden <ruizpaloma9@gmail.com>
2024-10-21 11:27:45 +0200

    exercise 9

--- arraysObjects/ejercicio7.js
+++ arraysObjects/ejercicio7.js
@@ -20,30 +20,57 @@ const productos = [
 ];
 
 // 5. Calculamos el total del carrito
-const totalCarrito = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+const totalCarrito = productos.reduce((acc, producto) => acc + producto.precio, 0);
 console.log(totalCarrito);
 
 // 6. Creamos un array de strings
 const palabras = ["hola", "mundo", "esto", "es", "JavaScript"];
 
 // 7. Concatenamos todas las palabras en una frase
-const frase = palabras.reduce((acumulador, palabra) => acumulador + " " + palabra);
-console.log(frase.trim());
+//const frase = palabras.reduce((acc, palabra) => acc + " " + palabra);
+//console.log(frase.trim()); // trim elimina los espacios en blanco
 
 // Otra forma de hacerlo
-/*const frase = palabras.reduce((acumulador, palabra) => 
-    acumulador ? acumulador + " " + palabra : palabra
+/*const frase = palabras.reduce((acc, palabra) => 
+    acc ? acc + " " + palabra : palabra
 );
-console.log(frase);*/ 
+console.log(frase); */
+
+/* condicion ternaria condicion ? valorSiVerdadero : valorSiFalso
+const edad = 18;
+const mensaje = (edad >= 18) ? "Eres mayor de edad" : "Eres menor de edad";
+console.log(mensaje)*/
+
 
 // 8. Contamos la frecuencia de cada palabra
 const texto = "hola mundo hola";
 const palabrasTexto = texto.split(" ");
-const frecuencia = palabrasTexto.reduce((acumulador, palabra) => {
-    acumulador[palabra] = (acumulador[palabra] || 0) + 1;
-    return acumulador;
+const frecuencia = palabrasTexto.reduce((acc, palabra) => {
+    acc[palabra] = (acc[palabra] || 0) + 1; // si acc[palabra] no existe(undefined), se usa 0 como valos inicial
+    return acc;
 }, {});
 console.log(frecuencia);
+
+/* primera iteracion 
+palabra = "hola"
+acc {} vacio
+acc[palabra] es undefined, así que usamos 0
+acc["hola"] = (undefined || 0 + 1) acc["hola"] = 0 + 1 
+acc = {"hola": 1}
+
+segunda iteracion 
+palabra = "mundo"
+acc {"hola": 1}
+acc["mundo"] es undefined 
+acc["mundo"] = (undefined || 0 + 1) acc["mundo"] = 0 + 1 
+acc = {"hola": 1, "mundo": 1}
+
+tercera iteracion 
+palabra = "hola"
+acc {"hola": 1, "mundo": 1}
+acc["hola"] es 1
+acc["hola"] = (1 || 0 + 1) acc["hola"] = 1 + 1 
+acc = {"hola": 2, "mundo": 1} */
 
 // 9. Aplanamos un array de arrays
 const arrayDeArrays = [[1,2], [3,4], [5]];
@@ -58,13 +85,13 @@ const personas = [
     { nombre: "María", edad: 24 }
 ];
 
-const agrupadasPorEdad = personas.reduce((acumulador, persona) => {
+const agrupadasPorEdad = personas.reduce((acc, persona) => {
     const edad = persona.edad;
-    if (!acumulador[edad]) {
-        acumulador[edad] = [];
+    if (!acc[edad]) {
+        acc[edad] = [];
     }
-    acumulador[edad].push(persona);
-    return acumulador;
+    acc[edad].push(persona);
+    return acc;
 }, {});
 
 console.log(agrupadasPorEdad);
